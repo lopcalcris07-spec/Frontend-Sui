149c3b3c8aeda3bc9c0a9d006ebb76791345f4a0
David Rivas <155179534+DvdRivas@users.noreply.github.com>
2025-11-17 10:35:32 -0700

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -25,7 +25,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 \. "$HOME/.nvm/nvm.sh"
 
-nvm install 24
+nvm install node
 
 npm install @mysten/sui.js @mysten/wallet-kit
 
